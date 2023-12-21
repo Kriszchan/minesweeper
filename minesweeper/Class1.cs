bb857dfd92eb35e76f2de0fb658a719a524bf126
Kriszchan <panyik.krisztian@students.jedlik.eu>
2023-12-21 17:23:51 +0100

    a

--- minesweeper/Class1.cs
+++ minesweeper/Class1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +11,73 @@ namespace minesweeper
 {
     public class eredmeny
     {
-        public static void palyagen(char [,] palya, bool[,] mines)
+         //alap
+        public static int[,] bombalerak(int[,] palyabelso, int minesdb)
+        {
+            Random r = new Random();
+            int x = minesdb;
+            while (x > 0)
+            {
+                palyabelso[r.Next(palyabelso.GetLength(0)), r.Next(palyabelso.GetLength(1))] = 9;
+                x -= 1;
+            }
+            return palyabelso;
+        }
+        public static int intcheck(string szveg, int max)
+        {
+            bool a;
+            int x;
+            do
+            {
+                Console.Write(szveg);
+                a = int.TryParse(Console.ReadLine(), out x);
+            }
+            while (a == false || x > max);
+            return x;
+        }
+        public static int[] lepescheck(int[] step, char[,] palya)
+        {
+            bool a = false;
+            bool b = false;
+            do
+            {
+                Console.Write("adja meg az első lépés koordinátáját pl: 9,6");
+                a = int.TryParse(Console.ReadLine().Split(",")[1], out step[0]);
+                b = int.TryParse(Console.ReadLine().Split(",")[1], out step[1]);
+            }
+            while ((step[0] < 1 || step[0] > palya.GetLength(0) || step[1] < 1 || step[1] > palya.GetLength(1)) || (a == false || b == false));
+            return step;
+        }
+        public static int[,] palyafeltolt(int[,] palyabelso)
+        {
+            for (int i = 0; i < palyabelso.GetLength(0); i++)
+            {
+                for (int j = 0; j < palyabelso.GetLength(1); j++)
+                {
+                    if (palyabelso[i, j] == 9)
+                    {
+                        for (int dx = -1; dx <= 1; dx++)
+                        {
+                            for (int dy = -1; dy <= 1; dy++)
+                            {
+                                int x = i + dx;
+                                int y = j + dy;
+
+                                if (!(dx == 0 && dy == 0) && x >= 0 && x < palyabelso.GetLength(0) && y >= 0 && y < palyabelso.GetLength(1) && palyabelso[x, y] == 9)
+                                {
+                                    palyabelso[x, y]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return palyabelso;
+        }
+
+        //gameplay
+
+        public static void palyakiir(char[,] palya, bool[,] mines)
         {
             for (int i = 0; i < palya.GetLength(0) + 3; i++)
             {
@@ -43,30 +112,47 @@ namespace minesweeper
                     }
                     else if (i >= 2)
                     {
-                        if (mines[i - 2, j] == false)
+                        if (palya[i,j] == '\0')
                         {
-                            Console.Write(" X |");
+                            Console.Write(" - |");
                         }
                         else
                         {
-                            Console.Write(" S |");
+                            Console.Write($" {palya[i,j]} |");
                         }
                     }
                 }
                 Console.Write("\n");
             }
         }
-         public static int intcheck(string szveg, int max)
+        public static int[,] lepes(int[,] palyabelso, char[,] palya, int[]step)
         {
-            bool a;
-            int x;
-            do
+            if (palyabelso[step[0], step[1]] != 9) palya[step[0], step[1]] = (char)palyabelso[step[0], step[1]];
+
+            for (int i = 0; i < palyabelso.GetLength(0); i++)
             {
-                Console.Write(szveg);
-                a = int.TryParse(Console.ReadLine(), out x);
+                for (int j = 0; j < palyabelso.GetLength(1); j++)
+                {
+                    if (i == step[0]&& j == step[1])
+                    {
+                        for (int dx = -1; dx <= 1; dx++)
+                        {
+                            for (int dy = -1; dy <= 1; dy++)
+                            {
+                                int x = i + dx;
+                                int y = j + dy;
+
+                                if (!(dx == 0 && dy == 0) && x >= 0 && x < palyabelso.GetLength(0) && y >= 0 && y < palyabelso.GetLength(1) && palyabelso[x, y] == 0)
+                                {
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            while (a == false || x > max);
-            return x;
         }
-    }
+
+
+}
 }
--- minesweeper/Program.cs
+++ minesweeper/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using minesweeper;
 int x;
-int minedb = 9;
+int minedb;
 bool a;
 Console.CursorVisible = false;
 bool isgameover = false;
@@ -10,9 +10,15 @@ Console.WriteLine("adja meg a pálya méretét: x*x maximum 50");
 x = eredmeny.intcheck("x= ", 50);
 char [,] palya = new char[x,x];
 bool[,] mines = new bool[palya.GetLength(0), palya.GetLength(1)];
+bool[,] nyitott = new bool[palya.GetLength(0), palya.GetLength(1)];
+int[,] palyabelso = new int[palya.GetLength(0), palya.GetLength(1)];
 Console.Write("adja meg a bombák számát:");
-minedb = eredmeny.intcheck("db= ", x);
+minedb = eredmeny.intcheck("db= ", 2*x);
 Console.Clear();
 eredmeny.palyagen(palya, mines);
 Console.Write("adja meg az első lépés koordinátáját pl: 9,6");
-string [] firststep = Console.ReadLine().Split();
+int[] step = new int[2];
+palyabelso = eredmeny.bombalerak(palyabelso, minedb);
+palyabelso = eredmeny.palyafeltolt(palyabelso);
+
+

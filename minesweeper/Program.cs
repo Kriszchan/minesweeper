3f29153e24a31f207fed41d5d2b124e2906ee2ae
Kriszchan <panyik.krisztian@students.jedlik.eu>
2024-01-02 09:58:44 +0100

    kesz

--- minesweeper/Program.cs
+++ minesweeper/Program.cs
@@ -21,55 +21,62 @@ namespace Arcade
                 string option = Console.ReadLine();
                 if (option == "1")
                 {
-                    int x;
-                    int minedb = 0;
-                    cella[,] palya;
-                    Console.WriteLine("adja meg a pálya méretét: x*x maximum 50");
-                    Console.WriteLine("Ha a mentett játékállást szeretné betölteni írjon 1-et");
-                    x = eredmeny.intcheck("x= ", 50);
-                    if (x == 1)
+                    Console.Clear();
+                    string valaszt = "i";
+                    while (valaszt == "i")
                     {
-                        palya = eredmeny.load();
-                        for (int i = 0; i < palya.GetLength(0); i++)
+                        int x;
+                        int minedb = 0;
+                        cella[,] palya;
+                        Console.WriteLine("adja meg a pálya méretét: x*x maximum 50");
+                        Console.WriteLine("Ha a mentett játékállást szeretné betölteni írjon 1-et");
+                        x = eredmeny.intcheck("x= ", 50);
+                        if (x == 1)
                         {
-                            for (int j = 0; j < palya.GetLength(1); j++)
+                            palya = eredmeny.load();
+                            for (int i = 0; i < palya.GetLength(0); i++)
                             {
-                                if (palya[i, j].belso == 9)
+                                for (int j = 0; j < palya.GetLength(1); j++)
                                 {
-                                    minedb++;
+                                    if (palya[i, j].belso == 9)
+                                    {
+                                        minedb++;
+                                    }
                                 }
                             }
-                        }
 
-                        eredmeny.palyakiir(palya);
-                    }
-                    else
-                    {
-                        palya = new cella[x, x];
-                        for (int i = 0; i < x; i++)
+                            eredmeny.palyakiir(palya);
+                        }
+                        else
                         {
-                            for (int j = 0; j < x; j++)
-
+                            palya = new cella[x, x];
+                            for (int i = 0; i < x; i++)
                             {
-                                palya[i, j] = new cella();
+                                for (int j = 0; j < x; j++)
+
+                                {
+                                    palya[i, j] = new cella();
+                                }
                             }
+
+                            Console.Write("adja meg a bombák számát:");
+                            minedb = eredmeny.intcheck("db= ", 2 * x);
+                            Console.Clear();
+                            eredmeny.palyakiir(palya);
+                            palya = eredmeny.bombalerak(palya, minedb);
+                            palya = eredmeny.palyafeltolt(palya);
                         }
+                        if (eredmeny.gameplayloop(palya, minedb) == true)
 
-                        Console.Write("adja meg a bombák számát:");
-                        minedb = eredmeny.intcheck("db= ", 2 * x);
-                        Console.Clear();
-                        eredmeny.palyakiir(palya);
-                        palya = eredmeny.bombalerak(palya, minedb);
-                        palya = eredmeny.palyafeltolt(palya);
-                    }
-                    if (eredmeny.gameplayloop(palya, minedb) == true)
-
-                    {
-                        Console.WriteLine("Gratulálunk ön nyert");
-                    }
-                    else
-                    {
-                        Console.WriteLine("GAME OVER");
+                        {
+                            Console.WriteLine("Gratulálunk ön nyert");
+                        }
+                        else
+                        {
+                            Console.WriteLine("GAME OVER");
+                        }
+                        Console.WriteLine("Új játék? i/n");
+                        valaszt = Console.ReadLine();
                     }
                 }
                 else if (option == "2")
